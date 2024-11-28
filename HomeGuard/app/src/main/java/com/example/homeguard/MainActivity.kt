86202850ee323ee2fc0908941c78a5108b850e9e
HaroldasT2 <D00256351@student.dkit.ie>
2024-11-28 20:48:01 +0000

    Updated MainActivity.kt
    Updated the MainActivity.kt by removing and adding new following features:
    - changed imagePainter to image to make it much simpler
    - removed people count functions, such variables as “peopleCount”, “yesterdayCount”, “averagePeople”, “roundUp” and “savedCounts”
    -added “isGreen” variable, which changes the colour of the microphone button to green when pressed and grey when pressed again.
    - added “isSwitchChecked” variable, which is just used to toggle on and off (does not do anything particular)
    
    There are also new images implemented to the HomeGuard drawables.
    
    If applied, this commit will update the MainActivity.kt and the HomeGuard folder.

--- HomeGuard/app/src/main/java/com/example/homeguard/MainActivity.kt
+++ HomeGuard/app/src/main/java/com/example/homeguard/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,9 +17,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -30,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.stringResource
@@ -45,12 +49,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HomeGuardTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomeGuardApp()
-                }
+                HomeGuardApp()
             }
         }
     }
@@ -75,7 +74,8 @@ fun HomeGuardApp() {
         ArticleCard(
             title = stringResource(R.string.title_jetpack_compose_tutorial),
             shortDescription = stringResource(R.string.compose_short_desc),
-            imagePainter = painterResource(R.drawable.bg_compose_background),
+            image1 = painterResource(R.drawable.bg_compose_background),
+            image2 = painterResource(R.drawable.microphone),
             modifier = Modifier.fillMaxSize().padding(16.dp)
         )
     }
@@ -85,17 +85,17 @@ fun HomeGuardApp() {
 private fun ArticleCard(
     title: String,
     shortDescription: String,
-    imagePainter: Painter,
+    image1: Painter,
+    image2: Painter,
     modifier: Modifier = Modifier,
 ) {
 
-    var peopleCount by remember { mutableStateOf(0) }
-    var yesterdayCount by remember { mutableStateOf(0) }
-    var averagePeople by remember { mutableStateOf(0.0) }
-    var roundUp by remember { mutableStateOf(false) }
 
-    // A List to store the previous saves for calculating the average
-    val savedCounts = remember { mutableListOf<Int>() }
+
+    var isGreen by remember { mutableStateOf(false) }
+    var isSwitchChecked by remember { mutableStateOf(false) }
+
+
 
     //Used to change the layout of the app
     Column(modifier = modifier,
@@ -105,99 +105,53 @@ private fun ArticleCard(
         Text(
             text = title,
             modifier = Modifier.padding(16.dp),
-            fontSize = 24.sp,
+            fontSize = 40.sp,
             textAlign = TextAlign.Center,
             color = Color.White
         )
 
-        Image(painter = imagePainter, contentDescription = null, Modifier.width(900.dp))
+        Image(painter = image1, contentDescription = null, modifier = Modifier.size(300.dp))
 
-        Text(
-            text = stringResource(R.string.people_count, peopleCount),
-            modifier = Modifier.padding(16.dp),
-            fontSize = 24.sp,
-            color = Color.White
-        )
+
 
         Button(
-            onClick = { peopleCount++ }, // Increments the people count
+            colors = ButtonDefaults.buttonColors(containerColor = if (isGreen) Color.Green else Color.Gray),
+            onClick = { isGreen = !isGreen },
             modifier = Modifier.padding(16.dp)
         ){
-            Text(text = stringResource(R.string.increment_button)) // Button text: "Add Person"
+            // Text(text = stringResource(R.string.increment_button)) // Button text: "Add Person"
+            Image(painter = image2, contentDescription = null, Modifier.width(100.dp))
         }
 
-        // A button to save the current count for today
-        Button(
-            onClick = {
-                savedCounts.add(peopleCount) // Save the current count
-                if (savedCounts.size == 1) {
-                    yesterdayCount = peopleCount // First save becomes yesterday's count
-                } else {
-                    // Calculates the average of all saves
-                    val total = savedCounts.sum().toDouble()
-                    averagePeople = total / savedCounts.size
-                }
-                peopleCount = 0 // Resets today's count after saving
-            },
-            modifier = Modifier.padding(16.dp)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = stringResource(R.string.save_button)) // Button text: "Save the Number"
-        }
-
-        // Switch to display average people when toggled
-        RoundTheTipRow(
-            roundUp = roundUp,
-            onRoundUpChanged = { roundUp = it },
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
-
-        // Displays the average number of people if the switch is on
-        if (roundUp) {
-            Text(
-                text = stringResource(R.string.average_people_count, averagePeople),
-                fontSize = 24.sp,
-                color = Color.White
+            Text(text = stringResource(R.string.show_average),
+                color = Color.White)
+            Switch(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+                checked = isSwitchChecked,
+                onCheckedChange = { isSwitchChecked = it }
             )
         }
+
+
 
         Spacer(modifier = Modifier.height(150.dp))
         Row (modifier = modifier) {
 
 
 
-            Text(
-                text = shortDescription,
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                textAlign = TextAlign.Justify,
-                color = Color.White
 
-            )
         }
     }
 
 }
 
-@Composable
-fun RoundTheTipRow(
-    roundUp: Boolean,
-    onRoundUpChanged: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(text = stringResource(R.string.show_average),
-            color = Color.White)
-        Switch(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.End),
-            checked = roundUp,
-            onCheckedChange = onRoundUpChanged
-        )
-    }
-}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
